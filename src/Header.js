3ecf290f075b0c65f91fb15e3d89c7d1424f1dcd
dpsl0726 <dpsl0726@gmail.com>
2023-08-16 19:53:53 +0900

    08162

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { Navbar, Button, Image } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
 class Header extends Component {
   state = {
-    buttonDisplay: "none"
+    buttonDisplay: "True"
   };
 
   render() {
@@ -19,22 +18,12 @@ class Header extends Component {
         <Navbar.Brand href="/"><div style={{width: '100%', textAlign: 'center', color: '#0066FF', fontSize: 40, fontFamily: 'Inter', fontWeight: '800', lineHeight: 2, wordWrap: 'break-word'}}>WON</div></Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            {/* <NavLink to="/mypage">
-              <Button style={buttonStyle} variant="primary">
-                회원정보 수정
-              </Button>
-            </NavLink> */}
             <NavLink to="/">
               <Button style={buttonStyle} variant="primary">
-                글목록
+                마이페이지
               </Button>
             </NavLink>
-            <NavLink to="/boardWrite">
-              <Button style={buttonStyle} variant="primary">
-                글쓰기
-              </Button>
-            </NavLink>
-            <Button style={buttonStyle} onClick={this.logout} variant="primary">
+            <Button style={buttonStyle} onClick={this.Logout} variant="primary">
               로그아웃
             </Button>
           </Navbar.Collapse>
